e2b912495c5ec12bc59599a2073baa0184801f63
Dan Simmons <daniel.simmons@nottingham.ac.uk>
2015-11-20 14:48:42 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ E_NOTROOT=87   # Non-root exit error.
 
 
 # Run as root, of course.
-sudo su
 if [ "$UID" -ne "$ROOT_UID" ]
 then
   echo "Must be root to run this script."
@@ -27,23 +26,24 @@ apt-get update
 apt-get install build-essential liblapack-dev libarpack++2-dev libopenblas-dev cmake libhdf5-dev zlib1g-dev unzip -y
 
 # download and install HDF5, MatIO and Armadillo
-mkdir build
-cd build || {
-  echo "Cannot change to necessary directory." >&2
-  exit $E_XCD;
-}
+mkdir build && cd build
 wget http://downloads.sourceforge.net/project/matio/matio/1.5.2/matio-1.5.2.zip http://sourceforge.net/projects/arma/files/armadillo-6.100.1.tar.gz
 unzip matio-1.5.2.zip
-cd matio-1.5.2
+cd matio-1.5.2 || {
+  echo "Cannot change to necessary directory. Check dowload link." >&2
+  exit $E_XCD;
+}
 ./configure --with-default-file-ver=7.3
 make install
 cd ..
 tar zxf armadillo-6.100.1.tar.gz
-cd armadillo-6.100.1
+cd armadillo-6.100.1 || {
+  echo "Cannot change to necessary directory. Check dowload link." >&2
+  exit $E_XCD;
+}
 ./configure
 make install
 cd ..
-
 
 exit 0
 #  A zero return value from the script upon exit indicates success
